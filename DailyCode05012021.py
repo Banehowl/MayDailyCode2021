19c653d30ebe6520f971752e7f725479a1dffbc1
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-05-01 09:55:19 -0700

    Roll a d00
    Daily Code for 05/01/2021

---
+++ DailyCode05012021.py
@@ -0,0 +1,13 @@
+# --------------------------------------
+# #	Daily Code	05/01/2021
+#   "Roll a d00" Lesson from edabit.com
+#   Coded by: Banehowl
+# --------------------------------------
+
+# Create code that rolls a d00
+
+import random
+
+dice_00 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 00]
+
+print(random.choice(dice_00))

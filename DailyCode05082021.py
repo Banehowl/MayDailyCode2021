41c81a016406de1c92c9beb2b0f98985891f1e46
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-05-08 08:24:02 -0700

    Curzon Numbers
    Daily Code for 05/08/2021

---
+++ DailyCode05082021.py
@@ -0,0 +1,40 @@
+# -------------------------------------------
+# #	Daily Code	05/08/2021
+#   "Curzon Numbers" Lesson from edabit.com
+#   Coded by: Banehowl
+# -------------------------------------------
+
+# In this challenge, establish if a given integer num is a Curzon number. If 1 plus 2 elevated to num is exactly
+# divisible by 1 plus 2 multiplied by num, then num is a Curzon number.
+#
+# Given a non-negative integer num, implement a function that returns True if num is a Curzon number, or
+# False otherwise.
+
+# is_curzon(5) -> True
+# # 2 ** 5 + 1 = 33
+# # 2 * 5 + 1 = 11
+# # 33 is a multiple of 11
+#
+# is_curzon(10) -> False
+# # 2 ** 10 + 1 = 1025
+# # 2 * 10 + 1 = 21
+# # 1025 is not a multiple of 21
+#
+# is_curzon(14) -> True
+# # 2 ** 14 + 1 = 16385
+# # 2 * 14 + 1 = 29
+# # 16385 is a multiple of 29
+
+def is_curzon(num):
+    firstNum = 2 ** num + 1
+    secondNum = 2 * num +1
+    thirdNum =  firstNum % secondNum
+
+    if thirdNum == 0:
+        return True
+    else:
+        return False
+
+print is_curzon(5)
+print is_curzon(10)
+print is_curzon(14)

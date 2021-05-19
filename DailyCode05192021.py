a4297195eaf4df5b2801773cd027659ec9477255
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-05-19 07:50:02 -0700

    Shuffle the Name (level 2)
    Daily Code for 05/18/2021

---
+++ DailyCode05192021.py
@@ -0,0 +1,25 @@
+# ---------------------------------------------
+# #	Daily Code	05/18/2021
+#   "Shuffle the Name (level 2)" Lesson from edabit.com
+#   Coded by: Banehowl
+# ---------------------------------------------
+
+# Create a function that takes a string (will be a person's first and last name) and returns a string with the
+# first and last name swapped.
+
+# name_shuffle("Donald Trump") -> "Trump Donald"
+# name_shuffle("Rosie O'Donnell") -> "O'Donnell Rosie"
+# name_shuffle("Seymour Butts") -> "Butts Seymour"
+
+def name_shuffle(txt):
+    stringTxt = str(txt)
+    arrayTxt = stringTxt.split()
+    reverseTxt = reversed(arrayTxt)
+    spaceTxt = " "
+    joinTxt = spaceTxt.join(reverseTxt)
+    smileyFace = joinTxt + " =)"
+    return smileyFace
+
+print name_shuffle("Donald Trump")
+print name_shuffle("Rosie O'Donnell")
+print name_shuffle("Seymour Butts")

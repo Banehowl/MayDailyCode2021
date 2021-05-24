a04d71905177490a28eb6de155288527f80cca40
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-05-24 07:20:57 -0700

    One Line: Minutes into Seconds
    Daily Code for 05/24/2021

---
+++ DailyCode05242021.py
@@ -0,0 +1,18 @@
+# --------------------------------------------------------------------
+# #	Daily Code	05/24/2021
+#   "Convert Minutes into Seconds (one line)" Lesson from edabit.com
+#   Coded by: Banehowl
+# --------------------------------------------------------------------
+
+# Write a function that takes an integer minutes and converts it to seconds.
+
+# convert(5) -> 300
+# convert(3) -> 180
+# convert(2) -> 120
+
+def convert(num):
+    return num * 60
+
+print convert(5)
+print convert(3)
+print convert(2)

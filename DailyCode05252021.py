a5520d407674859f7ffa90be8df314754b114091
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-05-25 07:57:48 -0700

    One Line: Return the Sum of Two Numbers
    Daily Code for 05/25/2021

---
+++ DailyCode05252021.py
@@ -0,0 +1,18 @@
+# --------------------------------------------------------------------
+# #	Daily Code	05/25/2021
+#   "Return the Sum of Two Numbers (one line)" Lesson from edabit.com
+#   Coded by: Banehowl
+# --------------------------------------------------------------------
+
+# Create a function that takes two numbers as arguments and return their sum.
+
+# addition(3, 2) -> 5
+# addition(-3, -6) -> -9
+# addition(7, 3) -> 10
+
+def addition(a, b):
+    return a + b
+
+print addition(3, 2)
+print addition(-3, -6)
+print addition(7, 3)

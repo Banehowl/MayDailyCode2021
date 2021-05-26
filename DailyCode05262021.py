bab15fe699e1107865c0e77a372558114632a133
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-05-26 07:28:08 -0700

    One Line: Next Number from the Integer
    Daily Code for 05/26/2021

---
+++ DailyCode05262021.py
@@ -0,0 +1,18 @@
+# --------------------------------------------------------------------------------------
+# #	Daily Code	05/26/2021
+#   "Return the Next Number from the Integer Passed (one line)" Lesson from edabit.com
+#   Coded by: Banehowl
+# --------------------------------------------------------------------------------------
+
+# Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+# addition(0) -> 1
+# addition(9) -> 10
+# addition(-3) -> -2
+
+def addition(num):
+    return num + 1
+
+print addition(0)
+print addition(9)
+print addition(-3)

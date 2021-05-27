daf5fb681c54abbac9201e3d8ed029691feae390
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-05-27 08:10:36 -0700

    One Line: Area of a Triangle
    Daily Code for 05/27/2021

---
+++ DailyCode05272021.py
@@ -0,0 +1,18 @@
+# --------------------------------------------------------------------------------------
+# #	Daily Code	05/27/2021
+#   "Area of a Triangle (one line)" Lesson from edabit.com
+#   Coded by: Banehowl
+# --------------------------------------------------------------------------------------
+
+# Write a function that takes the base and height of a triangle and return its area.
+
+# tri_area(3, 2) -> 3
+# tri_area(7, 4) -> 14
+# tri_area(10, 10) -> 50
+
+def tri_area(x, y):
+    return (x*y) / 2
+
+print tri_area(3, 2)
+print tri_area(7, 4)
+print tri_area(10, 10)

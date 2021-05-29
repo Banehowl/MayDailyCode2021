ec9d05ba4fb1eb422995fb4882572fb1235e9d08
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-05-29 09:03:14 -0700

    One Line: Convert Hours into Seconds
    Daily Code for 05/29/2021

---
+++ DailyCode05292021.py
@@ -0,0 +1,18 @@
+# ------------------------------------------------------------------
+# #	Daily Code	05/29/2021
+#   "Convert Hours into Seconds (one line)" Lesson from edabit.com
+#   Coded by: Banehowl
+# ------------------------------------------------------------------
+
+# Write a function that converts hours into seconds.
+
+# how_many_seconds(2) -> 7200
+# how_many_seconds(10) -> 36000
+# how_many_seconds(24) -> 86400
+
+def how_many_seconds(num):
+    return num * 3600
+
+print how_many_seconds(2)
+print how_many_seconds(10)
+print how_many_seconds(24)

679b6d5b5a07cdf32124296a2549989200fc92ae
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-05-30 08:11:56 -0700

    Is Black & White Ball Today?
    Daily Code for 05/30/2021

---
+++ DailyCode05302021.py
@@ -0,0 +1,14 @@
+# ------------------------------------------------------------------
+# #	Daily Code	05/30/2021
+#   "Is Black & White Ball Today?" Lesson from edabit.com
+#   Coded by: Banehowl
+# ------------------------------------------------------------------
+
+def ballToday(str):
+    if str == "yes":
+        return "Hell yeah!"
+    else:
+        return "No =("
+
+print ballToday("yes")
+print ballToday("no")
